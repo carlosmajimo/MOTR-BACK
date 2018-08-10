426de88f5b262f0101079a9eabd27d9ab242dce5
Carlos Jimenez <carlosmajimo@gmail.com>
2018-08-10 16:35:53 -0500

    Implementación del logger de winston

--- src/API/app.ts
+++ src/API/app.ts
@@ -1,27 +1,31 @@
 import express from "express";
 import compression from "compression";  // compresses requests
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
 import expressValidator from "express-validator";
 import flash from "express-flash";
 import lusca from "lusca";
 import path from "path";
+import { ENVIRONMENT } from "./Utils/secrets";
 
 // Import Routers (route handlers)
 import index from "./Routes/Views.routes";
 
+// Load environment variables from .env file, where API keys and passwords are configured
+dotenv.config({ path: ".env" });
+const environment = ENVIRONMENT;
 
 // Create Express server
 const app = express();
 
 // Express configuration
 app.set("port", process.env.PORT || 3000);
-app.set("Views", path.join(__dirname, "../Views"));
+app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "pug");
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
-// app.use(flash());
 app.use(lusca.xframe("SAMEORIGIN"));
 app.use(lusca.xssProtection(true));
 
