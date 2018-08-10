4568b8958106ddb54948b6203652615367ae4615
Carlos Jimenez <carlosmajimo@gmail.com>
2018-08-10 01:18:03 -0500

    Corrección del linter
    Cambié el indentado de espacios a tabulaciones y modifique los archivos que aun conservaban el indentado de espacio.

--- src/TCP-IP/app.ts
+++ src/TCP-IP/app.ts
@@ -1,24 +1,24 @@
 import net from "net";
 
 const tcpApp = net.createServer((c) => {
-    // 'connection' listener
-    console.log("client connected");
-    // console.log(c.eventNames());
+	// 'connection' listener
+	console.log("client connected");
+	// console.log(c.eventNames());
 
-    c.on("data", (data) => {
-        console.log(data.toString());
-    });
+	c.on("data", (data) => {
+		console.log(data.toString());
+	});
 
-    c.on("end", () => {
-        console.log("client disconnected");
-    });
+	c.on("end", () => {
+		console.log("client disconnected");
+	});
 
-    // c.write("hello\r\n");
-    c.pipe(c);
-});
+	// c.write("hello\r\n");
+	c.pipe(c);
+	});
 
 tcpApp.on("error", (err) => {
-    throw err;
+	throw err;
 });
 
 export default tcpApp;
