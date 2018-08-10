3e95d1adc28608c8178765588d82d1f34bea56fa
Carlos Jimenez <carlosmajimo@gmail.com>
2018-08-10 01:18:37 -0500

    Otra corrección del linter

--- src/API/server.ts
+++ src/API/server.ts
@@ -10,12 +10,12 @@ app.use(errorHandler());
  * Start Express server.
  */
 const server = app.listen(app.get("port"), () => {
-    console.log(
-        "   APIRest is running at http://localhost:%d in %s mode",
-        app.get("port"),
-        app.get("env")
-    );
-    console.log("   Press CTRL-C to stop\n");
+	console.log(
+		"   APIRest is running at http://localhost:%d in %s mode",
+		app.get("port"),
+		app.get("env")
+	);
+	console.log("   Press CTRL-C to stop\n");
 });
 
 export default server;
